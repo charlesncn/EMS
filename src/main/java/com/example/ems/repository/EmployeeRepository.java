898e5672108033c1940f0100d5cf1259b61d6862
charlesncn <charowcjn@gmail.com>
2023-08-31 17:23:04 +0300

    Fix: Use ReactiveCrudRepository

--- src/main/java/com/example/ems/repository/EmployeeRepository.java
+++ src/main/java/com/example/ems/repository/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.example.ems.repository;
 
 import com.example.ems.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+public interface EmployeeRepository extends ReactiveCrudRepository<EmployeeEntity, Integer> {
 
 }
--- src/main/java/com/example/ems/repository/ProjectRepository.java
+++ src/main/java/com/example/ems/repository/ProjectRepository.java
@@ -2,8 +2,9 @@ package com.example.ems.repository;
 
 import com.example.ems.entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<ProjectEntity, Integer> {
+public interface ProjectRepository extends ReactiveCrudRepository<ProjectEntity, Integer> {
 }
